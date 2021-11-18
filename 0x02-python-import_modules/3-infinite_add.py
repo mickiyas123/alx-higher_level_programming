53558abcc0fe5e1e84ea436a39e226e103ae1bbc
mickiyas123 <mikikinfe345@gmail.com>
2021-11-18 05:56:08 -0800

    Fixed an issue

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
         sum = 0
         for i in range(1, len(argv)):
             sum += int(argv[i])
-        print("{}".format(sum))    
+        print("{}".format(sum))
