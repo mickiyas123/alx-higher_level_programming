4f9e4bbcd1be5e7e87ca7dcaa41a804d2297bd23
mickiyas123 <mikikinfe345@gmail.com>
2021-11-22 18:26:04 -0800

    Fixed an issue

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    sum0 = 0
+    sum1 = 1
     if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        for i in range(2):
-            c = tuple(x + y for x, y in zip(tuple_a, tuple_b))
-    elif len(tuple_a) >= 2 and len(tuple_b) == 1:
-        new_list = list(tuple_b)
-        for i in range(len(new_list)):
-            new_list.append(0)
-        tuple_b = tuple(new_list)
-        for i in range(2):
-            c = tuple(x + y for x, y in zip(tuple_a, tuple_b))
+        sum0 = tuple_a[0] + tuple_b[0]
+        sum1 = tuple_a[1] + tuple_b[1]
     elif len(tuple_a) >= 2 and len(tuple_b) == 0:
         new_list = list(tuple_b)
-        for i in range(2):
-            new_list.append(0)
+        new_list += [0, 0]
         tuple_b = tuple(new_list)
-        for i in range(2):
-            c = tuple(x + y for x, y in zip(tuple_a, tuple_b))
+        sum0 = tuple_a[0] + tuple_b[0]
+        sum1 = tuple_a[1] + tuple_b[1]
+    elif len(tuple_a) >= 2 and len(tuple_b) == 1:
+        new_list = list(tuple_b)
+        new_list += [0]
+        tuple_b = tuple(new_list)
+        sum0 = tuple_a[0] + tuple_b[0]
+        sum1 = tuple_a[1] + tuple_b[1]
     elif len(tuple_a) == 0 and len(tuple_b) >= 2:
         new_list = list(tuple_a)
-        for i in range(2):
-            new_list.append(0)
+        new_list += [0, 0]
         tuple_a = tuple(new_list)
-        for i in range(2):
-            c = tuple(x + y for x, y in zip(tuple_a, tuple_b))
+        sum0 = tuple_a[0] + tuple_b[0]
+        sum1 = tuple_a[1] + tuple_b[1]
     elif len(tuple_a) == 1 and len(tuple_b) >= 2:
         new_list = list(tuple_a)
-        for i in range(1):
-            new_list.append(0)
+        new_list += [0]
         tuple_a = tuple(new_list)
-        for i in range(2):
-            c = tuple(x + y for x, y in zip(tuple_a, tuple_b))
-    return c    
+        sum0 = tuple_a[0] + tuple_b[0]
+        sum1 = tuple_a[1] + tuple_b[1]
+    return (sum0, sum1)
