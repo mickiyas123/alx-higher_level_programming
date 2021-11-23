781d9ff3b3bab9cfea2ffdc022c4ba5a7e9e09f6
mickiyas123 <mikikinfe345@gmail.com>
2021-11-22 17:39:13 -0800

    Fixed an issue

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,30 +1,28 @@
 #include <Python.h>
-#include <object.h>
-#include <listobject.h>
 
 /**
- * print_python_list_info - function that prints some basic info 
- * about Python lists
- * @p: Address of thr list
+ * print_python_list_info -  function that prints some 
+ * basic info about Python lists
+ * @p: Address to array object
  *
- * Return: Always 0
+ * Return: ALways 0.
  */
 
 void print_python_list_info(PyObject *p)
 {
-	int i, len;
-	char *el_type;
+	int i, len, msize;
+	PyObject *obj;
 	
-	if (p != NULL)
-	{
-		len = (int)((PyVarObject *)p)->ob_size;
-		printf("[*] Size of the Python List = %d", len);
-		printf("[*] Allocated = %d", ((PyListObject *)p)->allocated);
+	len = Py_SIZE(P);
+	msize = ((PyListObject *)p)->allocated
 
-		for (i = 0; i < len; i++)
-		{
-			el_type = (((PyListObject *)p)->ob_item + 1)->ob_type->tp_name;
-			printf("Element %d: %s\n", i, el_type);
-		}
+	printf("[*] Size of the Python List = %d\n", len);
+	printf("[*] Allocated = %d\n", msize);
+
+	for (i = 0; i < len; i++)
+	{
+		print("Element 0: %d", i);
+		obj = PyList_GetItem(p, i);
+		print("%s\n", Py_TYPE(obj)->tp_name);
 	}
 }
