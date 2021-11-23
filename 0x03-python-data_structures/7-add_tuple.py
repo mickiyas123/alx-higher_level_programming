e88492e1b421e13ef3b1ed023b740e0e6a9b2fd0
mickiyas123 <mikikinfe345@gmail.com>
2021-11-22 18:37:58 -0800

    Fixed an issue

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -29,4 +29,40 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = tuple(new_list)
         sum0 = tuple_a[0] + tuple_b[0]
         sum1 = tuple_a[1] + tuple_b[1]
+    elif len(tuple_a) == 0 and (tuple_b) == 0:
+        new_list = list(tuple_a)
+        new_list1 = list(tuple_b)
+        new_list += [0, 0]
+        new_list1 += [0, 0]
+        tuple_a = tuple(new_list)
+        tuple_b = tuple(new_list1)
+        sum0 = tuple_a[0] + tuple_b[0]
+        sum1 = tuple_a[1] + tuple_b[1]
+    elif len(tuple_a) == 1 and (tuple_b) == 1:
+        new_list = list(tuple_a)
+        new_list1 = list(tuple_b)
+        new_list += [0]
+        new_list1 += [0]
+        tuple_a = tuple(new_list)
+        tuple_b = tuple(new_list1)
+        sum0 = tuple_a[0] + tuple_b[0]
+        sum1 = tuple_a[1] + tuple_b[1]
+    elif len(tuple_a) == 1 and (tuple_b) == 0:
+        new_list = list(tuple_a)
+        new_list1 = list(tuple_b)
+        new_list += [0]
+        new_list1 += [0, 0]
+        tuple_a = tuple(new_list)
+        tuple_b = tuple(new_list1)
+        sum0 = tuple_a[0] + tuple_b[0]
+        sum1 = tuple_a[1] + tuple_b[1]
+    elif len(tuple_a) == 0 and (tuple_b) == 1:
+        new_list = list(tuple_a)
+        new_list1 = list(tuple_b)
+        new_list += [0, 0]
+        new_list1 += [0]
+        tuple_a = tuple(new_list)
+        tuple_b = tuple(new_list1)
+        sum0 = tuple_a[0] + tuple_b[0]
+        sum1 = tuple_a[1] + tuple_b[1]
     return (sum0, sum1)
