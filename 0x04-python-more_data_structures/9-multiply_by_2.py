e9a18508affab21f13bfb35ee1104b0761d58ad3
mickiyas123 <mikikinfe345@gmail.com>
2021-11-24 03:22:58 -0800

    Fixed an issue

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,4 @@ def multiply_by_2(a_dictionary):
     b_dictionary = a_dictionary.copy()
     for key in b_dictionary:
         b_dictionary[key] *= 2
-    return b_dictionary    
-
+    return b_dictionary
