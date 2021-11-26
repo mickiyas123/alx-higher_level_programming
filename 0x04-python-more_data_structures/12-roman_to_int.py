96f41d6f6217022c32de915ea256bf711492f7ac
mickiyas123 <mikikinfe345@gmail.com>
2021-11-26 02:09:10 -0800

    fixed an issue

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string:
+    if type(roman_string) == str and roman_string != None:
         my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         total = 0
         prev = 0
@@ -8,7 +8,7 @@ def roman_to_int(roman_string):
         for i in range(len(roman_string)):
             cur = my_dict[roman_string[i]]
             if cur > prev:
-                total += cur - 2 * prev
+                total += cur - (2 * prev)
             else:
                 total += cur
             prev = cur
