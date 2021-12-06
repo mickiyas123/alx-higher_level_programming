904b8972d949b6e41cab96c6e6d0862bfb67f1c3
mickiy123 <mikikinfe345@gmail.com>
2021-12-06 17:51:24 +0300

    Fixed an issue

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         x = True
     except:
-        print("Exception:",sys.exc_info()[1], file=sys.stderr)
+        print("Exception:", sys.exc_info()[1], file=sys.stderr)
         x = False
     return x
