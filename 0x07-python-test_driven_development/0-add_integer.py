9543cd3bf31505abf4f8b9cdfa84be268f851b66
mickiy123 <mikikinfe345@gmail.com>
2021-12-17 10:43:44 +0300

    Fixed an issue

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+""" add_integer
+    Adds two integer together
+"""
 
-
-# 0-add_integer.py
 
 def add_integer(a, b=98):
     """Function that adds two int.
@@ -15,6 +16,7 @@ def add_integer(a, b=98):
         Return:
                sum of a and b
     """
+
     if type(a) is int or type(a) is float:
         a = int(a)
     else:
