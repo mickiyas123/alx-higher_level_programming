1cd97cb8496202c367a52d1f63f091c204074267
mickiy123 <mikikinfe345@gmail.com>
2021-12-20 23:57:04 +0300

    Fixed an issue

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -13,4 +13,6 @@ class MyList(list):
         print sorted list
 
         """
-        print(sorted(self))
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
