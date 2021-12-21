5d2aa108d0edd9366267d6a3ea41ebc9cb4e726c
mickiy123 <mikikinfe345@gmail.com>
2021-12-21 22:01:05 +0300

    Fixed an issue

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
+import sys
+
 """ Module 7-add_item Load, add, save to json file """
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-import sys
 
 new_list = []
 
