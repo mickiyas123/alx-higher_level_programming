26c38de39d56d4e569833ebb5cc5b0154a7281db
mickiy123 <mikikinfe345@gmail.com>
2022-02-01 18:55:46 +0300

    Task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- script that prints the full description of the table from a databse
-SELECT * FROM INFORMATION_SCHEMA.Columns WHERE table_name = 'first_table';
+SHOW CREATE table hbtn_0c_0.first_table;
