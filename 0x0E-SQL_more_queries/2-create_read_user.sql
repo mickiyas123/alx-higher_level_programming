0610b643eee3062ea9d335120724b72b96bc6f4c
mickiy123 <mikikinfe345@gmail.com>
2022-02-02 17:40:58 +0300

    Fixed an issue

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@
 --	The script will not fail, If the database hbtn_0d_2 already exists
 --	The script will not fail, If the user user_0d_2 already exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd'
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
