e68ec42af6b428ebe01f920e381baaedfc38aeab
mickiy123 <mikikinfe345@gmail.com>
2022-02-25 13:39:49 +0300

    Add: add advance task 102

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,2 @@
--- Import in hbtn_0c_0 database this table dump: sql file
--- script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
-mysqldump -u root hbtn_0c_0 -p < temperatures.sql
+-- script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month IN (7,8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
