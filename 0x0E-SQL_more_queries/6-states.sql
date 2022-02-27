216931b262944c2cb820ff408444f1fc42cca45f
mickiy123 <mikikinfe345@gmail.com>
2022-02-27 12:09:16 +0300

    Update: used easier way to impliment task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -4,5 +4,4 @@
 --		      name VARCHAR(256) can’t be null
 -- The script will not fail, if the database hbtn_0d_usa already exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
