b4420051a93077985008fe4cbe558de586b7ae49
mickiy123 <mikikinfe345@gmail.com>
2022-02-27 14:13:58 +0300

    Update: use easier way to impliment task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,3 @@
 -- Import the database dump from hbtn_0d_tvshows
 -- script that lists all shows contained in hbtn_0d_tvshows without a genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT ts.title, tsg.genre_id FROM tv_shows ts LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id WHERE genre_id IS NULL ORDER BY ts.title, tsg.genre_id;
