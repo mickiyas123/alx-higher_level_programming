9d361d85e82f2aacefc1665fdf6bd198f8d8b7bf
mickiy123 <mikikinfe345@gmail.com>
2022-03-03 11:45:09 +0300

    Fix: use one select statment

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,3 @@
 -- Import the database dump from hbtn_0d_tvshows
 -- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-SELECT name FROM tv_genres WHERE id IN (SELECT genre_id FROM tv_show_genres WHERE show_id = (SELECT id FROM tv_shows WHERE title='Dexter')) ORDER BY name;
+SELECT tg.name FROM tv_genres tg JOIN tv_show_genres tsg ON tg.id = tsg.genre_id JOIN tv_shows ts ON tsg.show_id = ts.id WHERE ts.title = 'Dexter' ORDER BY tg.name;
