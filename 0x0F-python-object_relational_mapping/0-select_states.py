f86f51c66227c9f5e96dbff0fb4d4fc75725a657
mickiy123 <mikikinfe345@gmail.com>
2022-03-04 11:25:58 +0300

    Add: complete task 1

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" module that lists all states in hbtn_0e_0_usa databse """
+
+# Standard Library imports
+import sys
+
+
+# related third party imports
+import MySQLdb as sql
+
+
+user = sys.argv[1]
+passwd = sys.argv[2]
+database = sys.argv[3]
+
+conn = sql.connect(
+        host='localhost',
+        port=3306,
+        user=user,
+        passwd=passwd,
+        db=database)
+
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM states")
+
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+conn.close()
