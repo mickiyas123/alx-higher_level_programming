4759dab7e85d1b45c5a97a0a8c06ac225a496613
mickiy123 <mikikinfe345@gmail.com>
2022-03-04 11:37:08 +0300

    Fix: add condtion to be satisfied

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
 """ module that lists all states in hbtn_0e_0_usa databse """
 
-# Standard Library imports
-import sys
+if __name__ == '__main__':
+    # Standard Library imports
+    import sys
 
+    # related third party imports
+    import MySQLdb as sql
 
-# related third party imports
-import MySQLdb as sql
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
 
+    conn = sql.connect(
+            host='localhost',
+            port=3306,
+            user=user,
+            passwd=passwd,
+            db=database)
 
-user = sys.argv[1]
-passwd = sys.argv[2]
-database = sys.argv[3]
+    cur = conn.cursor()
 
-conn = sql.connect(
-        host='localhost',
-        port=3306,
-        user=user,
-        passwd=passwd,
-        db=database)
+    cur.execute("SELECT * FROM states ORDER BY id")
 
-cur = conn.cursor()
+    rows = cur.fetchall()
 
-cur.execute("SELECT * FROM states")
+    for row in rows:
+        print(row)
 
-rows = cur.fetchall()
-
-for row in rows:
-    print(row)
-
-cur.close()
-conn.close()
+    cur.close()
+    conn.close()
