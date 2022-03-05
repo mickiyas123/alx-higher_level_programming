5dcc85f55317e28fcc3dbf28ad4c3783e75cbebd
mickiy123 <mikikinfe345@gmail.com>
2022-03-05 19:43:32 +0300

    Docs: update documentation

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,7 +3,6 @@
 """
 
 # related third party imports
-
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -12,6 +11,15 @@ Base = declarative_base()
 
 
 class State(Base):
+    """ State class:
+                inherits from Base Tips
+                links to the MySQL table states
+                class attribute id that represents a column of
+                an auto-generated, unique integer, can’t be null and
+                is a primary key class attribute name that represents
+                a column of a string with maximum 128 characters
+                and can’t be null
+    """
     __tablename__ = 'states'
 
     id = Column(Integer(), primary_key=True, unique=True, nullable=False)
