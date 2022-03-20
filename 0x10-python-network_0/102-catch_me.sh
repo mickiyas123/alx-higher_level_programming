d89b3717dfef86276897d5e725a4228daaefe719
mickiy123 <mikikinfe345@gmail.com>
2022-03-20 22:19:16 +0300

    Fix: implement another way

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!
-curl -s -X PUT -H "Origin: You got me!" 0.0.0.0:5000/catch_me
+curl -s -L 0.0.0.0:5000/catch_me | sed 's/.*/You got me!/'
