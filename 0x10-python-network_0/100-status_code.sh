d9a3766ce0131d4a6e9a04e6610df1abc6013a78
mickiy123 <mikikinfe345@gmail.com>
2022-03-20 20:09:43 +0300

    Refactor: use small line of code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sI "$1" | awk 'NR==1 {print $2}'
+curl -sw '%{response_code}\n' -o /dev/null "$1"
