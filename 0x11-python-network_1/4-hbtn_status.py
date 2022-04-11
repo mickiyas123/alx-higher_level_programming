537fae944b4ea19bdbb48a8019bdd0eec9770056
mickiy123 <mikikinfe345@gmail.com>
2022-04-11 16:49:07 +0300

    Style: use pep8 standard

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,8 +5,7 @@ if __name__ == '__main__':
     import requests
 
     r = requests.get("https://alx-intranet.hbtn.io/status")
-    
+
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
-
