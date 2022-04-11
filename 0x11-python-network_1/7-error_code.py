8114aecc0d75cade67d20def6a66b73c5bf527c3
mickiy123 <mikikinfe345@gmail.com>
2022-04-11 17:55:36 +0300

    Fix: remove repeated imports

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,8 +2,6 @@
 """Python script that takes in a URL, sends a request to the URL and
 displays the body of the response"""
 
-import requests
-from sys import argv
 
 if __name__ == "__main__":
     import requests
