e2065d2eb1b5efd809c895ab44f168631d35bf3b
mickiy123 <mikikinfe345@gmail.com>
2022-04-11 17:04:21 +0300

    Fix: add condition

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,4 +8,7 @@ if __name__ == '__main__':
     import sys
 
     r = requests.get(sys.argv[1])
-    print(r.headers['X-Request-Id'])
+    if r.headers['X-Request-Id']:
+        print(r.headers['X-Request-Id'])
+    else:
+        pass
