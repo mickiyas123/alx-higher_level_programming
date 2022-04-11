c4a6edae9fe05300858521f7b13e212e7db5d0d9
mickiy123 <mikikinfe345@gmail.com>
2022-04-11 16:33:23 +0300

    Style: change some lines

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,12 +3,12 @@
 displays the body of the response"""
 
 if __name__ == "__main__":
-    import urllib.request
+    from urllib.request import urlopen
     from urllib.error import HTTPError
     import sys
 
     try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            print(response.read().decode("utf-8", "replace"))
+        with urlopen(sys.argv[1]) as response:
+            print(response.read().decode("UTF-8"))
     except HTTPError as e:
         print("Error code: {}".format(e.code))
