cfd72c4dc10e9ccf64933069df892d96145d23ed
mickiy123 <mikikinfe345@gmail.com>
2022-04-11 22:03:23 +0300

    Update: use different way of implimentation

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""Takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter"""
+"""Python script that takes in a letter and sends a POST request to
+   http://0.0.00.0:5000/search_user with the letter as a parameter
+"""
 
 
 if __name__ == "__main__":
     import requests
     import sys
 
-    q = sys.argv[1] if len(sys.argv) > 1 else ""
-    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
-    try:
-        dic = r.json()
-        if dic == {}:
-            print('No result')
+    if len(sys.argv) == 2:
+        if sys.argv[1].isdigit():
+            print("No result")
         else:
-            print("[{}] {}".format(dic.get('id'), dic.get('name')))
-    except ValueError:
-        print('Not a valid JSON')
+            response = requests.post(
+                    "http://0.0.0.0:5000/search_user",
+                    data={'q': sys.argv[1]})
+            if response:
+                print("[{}] {}".format(
+                    response.json()['id'],
+                    response.json()["name"]))
+                else:
+                    print("Not a valid JSON")
+    elif len(sys.argv) == 1:
+        print("No result")
