8e808beb2f18229241ead923d5f5abc2b819a333
mickiy123 <mikikinfe345@gmail.com>
2022-04-11 16:44:33 +0300

    Style: change comment and print format

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+""" Python script that fetches https://intranet.hbtn.io/status"""
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     import requests
 
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
 
-    print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
