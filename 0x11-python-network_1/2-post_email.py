9f071d199583f3025828529133e2ffeeffc485aa
mickiy123 <mikikinfe345@gmail.com>
2022-04-11 16:20:47 +0300

    Fix: use data encoding

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     email = sys.argv[2]
     values = {'email': '{}'.format(email)}
     data = urllib.parse.urlencode(values)
+    data = data.encode('UTF-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode("UTF-8"))
