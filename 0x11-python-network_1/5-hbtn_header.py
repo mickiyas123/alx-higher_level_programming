b44aab9c1c5fed3e2cca229c5ca2a10b10b81c78
mickiy123 <mikikinfe345@gmail.com>
2022-04-11 17:01:20 +0300

    Style: change comment and print format

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request to the URL and displays the value of the
-X-Request-Id variable found in the header of the response"""
+"""Python script that takes in a URL, sends a request to the URL and displays
+   the value of the variable X-Request-Id in the response header
+"""
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     import requests
     import sys
 
     r = requests.get(sys.argv[1])
-    print(r.headers.get('X-Request-Id'))
+    print(r.headers['X-Request-Id'])
