6897ff33f06e3c979c1e2d332df949921660da04
mickiy123 <mikikinfe345@gmail.com>
2022-04-12 10:38:18 +0300

    Add: vomplete task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Python script that takes your GitHub credentials (username and password)
+   and uses the GitHub API to display your id
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    user = sys.argv[1]
+    token = sys.argv[2]
+
+    headers = {'Authorization': token}
+    response = requests.get(
+            "https://api.github.com/users/{}"
+            .format(user),
+            headers=headers)
+    if response.json():
+        print(response.json()["id"])
+    else:
+        print("None")
