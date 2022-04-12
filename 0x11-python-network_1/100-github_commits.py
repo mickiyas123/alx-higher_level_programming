8ce022faed915fa2376ecfc4f031b4ce6cc616c4
mickiy123 <mikikinfe345@gmail.com>
2022-04-12 12:52:47 +0300

    Update: use different way

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,11 +7,18 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'
-                     .format(sys.argv[2], sys.argv[1]))
-    if r.status_code >= 400:
-        print('None')
-    else:
-        for com in r.json()[:10]:
-            print("{}: {}".format(com.get('sha'),
-                                  com.get('commit').get('author').get('name')))
+    owner = sys.argv[1]
+    repo = sys.argv[2]
+
+    response = requests.get(
+            "https://api.github.com/repos/{}/{}/commits"
+            .format(owner, repo))
+
+    auth_json = response.json()
+    count = 0
+
+    for i in auth_json:
+        print("{}: {}".format(i["sha"], i["commit"]["author"]["name"]))
+        count = count + 1
+        if (count == 10):
+            break
