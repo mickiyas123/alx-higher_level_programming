906113c559864c02e1e1bc17f3862bdc3683d08c
mickiy123 <mikikinfe345@gmail.com>
2022-04-16 13:41:40 +0300

    Refactor: use iterator

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-let i;
-
-for (i = 0; i < myArray.length; i++) {
-  console.log(myArray[i]);
+for (const string of myArray) {
+  console.log(string);
 }
