a0e6eecfa5385fc0816fb51d77d3c86d926c9aaa
mickiy123 <mikikinfe345@gmail.com>
2022-04-17 12:39:23 +0300

    Fix: use for loop

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,23 +5,24 @@ request('https://jsonplaceholder.typicode.com/users', function (error, response,
     console.log(error);
   }
   const x = {};
-  let k = 0;
-  for (const user of JSON.parse(body)) {
+  let c = 0;
+  const users = JSON.parse(body);
+  for (let k = 0; k < users.length; k++) {
     request(process.argv[2], function (error, response, body) {
       if (error) {
         console.log(error);
       }
       let i = 0;
       for (const todo of JSON.parse(body)) {
-        if (user.id === todo.userId) {
+        if (users[k].id === todo.userId) {
           if (todo.completed === true) {
             i++;
           }
         }
       }
-      k++;
-      x[user.id] = i;
-      if (k === 10) {
+      c++;
+      x[users[k].id] = i;
+      if (c === users.length) {
         console.log(x);
       }
     });
